df6b1d793724430a7fe96f79fc60dccf8120787f
ateebplayz <challengebudies9@gmail.com>
2024-01-05 00:00:14 +0500

    Fixed port

--- backend/V2/MongoUpload/index.mjs
+++ backend/V2/MongoUpload/index.mjs
@@ -179,6 +179,6 @@ app.post('/api/margins/set', async (req,res) => {
         } else return res.json({msg: 'Margin ID Not Found. Check margins @ https://api.muscatbullion.com:5001/api/margins', code: 0})
     } else return res.json({msg: 'User Credentials/Token Invalid', code: 0})
 })
-app.listen(5001, () => {
+app.listen(5000, () => {
     console.log('App listening on ' + config.port)
 })
